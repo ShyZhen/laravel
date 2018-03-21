7c134d937195a632b5a24cab11d2b5c9d9fddf19
huaixiu.zhen <huaixiu.zhen@gmail.com>
2018-03-21 18:36:38 +0800

    view Test

---
+++ app/Http/Controllers/Home/ViewController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author huaixiu.zhen
+ * http://litblc.com
+ * User: litblc
+ * Date: 2018/3/21
+ * Time: 18:30
+ */
+
+namespace App\Http\Controllers\Home;
+
+
+class ViewController
+{
+    public function index()
+    {
+        dd('view.');
+    }
+}
