156d6c4959fc35697d8166e9921d45f1641a29c6
huaixiu.zhen <huaixiu.zhen@gmail.com>
2018-03-30 18:45:32 +0800

    css and js and gulp

--- resources/views/__layout/layout.blade.php
+++ resources/views/__layout/layout.blade.php
@@ -1,8 +1,12 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="http://apps.bdimg.com/libs/jquery/1.9.1/jquery.js"></script>
+    {{--<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
+    {{--<script src="http://apps.bdimg.com/libs/jquery/1.9.1/jquery.js"></script>--}}
+
+    {{--默认集成bootcss和vue脚手架--}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}"></script>
 </head>
 <body>
     @section('sidebar')
