d6948527baf3317388e162091ce713da85fb3076
huaixiu.zhen <huaixiu.zhen@gmail.com>
2018-05-20 13:03:53 +0800

    add cors middleware

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,14 @@ Route::get('/user', function (Request $request) {
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     // no auth:api
-    $api->group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function($api) {
+    $api->group(['prefix' => 'v1', 'middleware' => 'cors', 'namespace' => 'App\Http\Controllers\Api\V1'], function($api) {
         $api->post('login', 'AuthController@login');
         $api->post('register-code', 'AuthController@registerCode');
         $api->post('register', 'AuthController@register');
         $api->post('password-code', 'AuthController@passwordCode');
         $api->post('password', 'AuthController@password');
     });
-    $api->group(['prefix' => 'v1','middleware' => 'auth:api', 'namespace' => 'App\Http\Controllers\Api\V1'], function($api) {
+    $api->group(['prefix' => 'v1','middleware' => [ 'cors', 'auth:api'], 'namespace' => 'App\Http\Controllers\Api\V1'], function($api) {
         $api->get('user', 'AuthController@userInfo');
         $api->get('logout', 'AuthController@logout');
     });
